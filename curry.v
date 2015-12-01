e7d099f883992ee903239a41546d8c0bc56f5f72
Eric Bailey <eric@ericb.me>
2015-12-01 01:18:01 -0600

    Add dead-end proof attempt

--- curry.v
+++ curry.v
@@ -3,3 +3,8 @@ Parameters A B C : Set.
 (* A function f : A * B -> C is equivalent to a function A -> (B -> C). *)
 Definition curry   (f : A * B -> C) := fun a => fun b => f (a, b).
 Definition uncurry (g : A -> B -> C) := fun p => g (fst p) (snd p).
+
+Theorem attempt : forall f, uncurry (curry f) = f.
+Proof.
+  intros.
+  unfold curry, uncurry.
