2287c158e2cd09e090d8751a19ef3de728f4a84e
Eric Bailey <eric@ericb.me>
2015-12-01 01:17:10 -0600

    Add {un,}curry definitions

---
+++ curry.v
@@ -0,0 +1,5 @@
+Parameters A B C : Set.
+
+(* A function f : A * B -> C is equivalent to a function A -> (B -> C). *)
+Definition curry   (f : A * B -> C) := fun a => fun b => f (a, b).
+Definition uncurry (g : A -> B -> C) := fun p => g (fst p) (snd p).
